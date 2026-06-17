6655061fdde863adc78da286b8249760af454a0e
Erik-Pma <56539093+Erik-Pma@users.noreply.github.com>
2026-06-17 12:23:04 -0700

    trying to link

--- myjavascript.js
+++ myjavascript.js
@@ -1,11 +1,7 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.carousel');
-  var instances = M.Carousel.init(elems, {
-    duration: 200,      // Transition transition time
-    dist: -100,         // Perspective zoom. Play with this for card depth!
-    shift: 0,           // Space between cards
-    padding: 0,         // Padding between cards
-    numVisible: 5,      // Number of visible items
-    indicators: true    // Shows the little slide dots at the bottom
+$(document).ready(function(){
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true,
+    duration: 200
   });
 });
