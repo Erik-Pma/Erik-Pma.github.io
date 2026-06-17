47ee7d1cd3242458879990ec015649b656cc0a0c
Erik-Pma <56539093+Erik-Pma@users.noreply.github.com>
2026-06-17 11:49:48 -0700

    trying to link items

--- myjavascript.js
+++ myjavascript.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.carousel');
   var instances = M.Carousel.init(elems, {
-    // Options go here
-    dist: -50, // Perspective zoom (negative values bring back cards forward)
-    shift: 0,  // Spacing between items
-    padding: 20
+    duration: 200,      // Transition transition time
+    dist: -100,         // Perspective zoom. Play with this for card depth!
+    shift: 0,           // Space between cards
+    padding: 0,         // Padding between cards
+    numVisible: 5,      // Number of visible items
+    indicators: true    // Shows the little slide dots at the bottom
   });
 });
